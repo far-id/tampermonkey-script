3f9b47664bc0a93aed3aa7b1ca9014a46d9d94ed
far-id <fcrizkywijaya@gmail.com>
2025-10-13 16:13:16 +0700

    update ext icon for anime page on livechart.me

--- livechart.me/add-mal-rating-on-anime-page.js
+++ livechart.me/add-mal-rating-on-anime-page.js
@@ -5,7 +5,7 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.livechart.me/anime/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=livechart.me
 // @grant        GM_xmlhttpRequest
 // @connect      api.myanimelist.net
 // ==/UserScript==
